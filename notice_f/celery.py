1929b2d0aa905d991697cbd696063b41b0b56cd4
art <tym3@yandex.ru>
2024-02-21 15:04:00 +0500

    celery

--- notice_f/celery.py
+++ notice_f/celery.py
@@ -3,7 +3,7 @@ import config
 import logging.config
 from celery import Celery
 from celery.schedules import crontab
-
+from api.tasks import start_mailing
 app = Celery("notice_f")
 
 logging.config.dictConfig(config.dict_config_celery)
@@ -20,7 +20,7 @@ app.conf.update(
 
 app.conf.beat_schedule = {
     "start_mailing_every_5_minutes": {
-        "task": "api.tasks.start_mailing",  # Путь к вашей функции
+        "task": start_mailing,  # Путь к вашей функции
         "schedule": crontab(minute="*/5"),  # Расписание: каждые 5 минут
     },
 }
